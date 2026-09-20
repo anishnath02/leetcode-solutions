093027181937db94dda39944d092b147267bd7fa
Anish Nath <anishnath2002@gmail.com>
2026-09-20 06:12:21 +0530

    Create 1-two-sum.py

---
+++ Arrays/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+
+        for idx, val in enumerate(nums):
+            diff = target - val
+
+            if diff in hashmap:
+                return [idx, hashmap[diff]]
+            
+            hashmap[val] = idx
